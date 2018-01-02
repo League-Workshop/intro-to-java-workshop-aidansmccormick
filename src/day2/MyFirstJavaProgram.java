44e4880525d162743197cbcaffc10235d6d7b7dc
GIBBY <aidansmccormick@hotmail.com>
2018-01-02 14:58:10 -0800

    day 1 workshop

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -6,10 +6,18 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
-		
 		// START HERE
-	
+	System.out.println("HELLO WORLD!!!");
+		Robot dalek = new Robot();
 		
+		dalek.penDown();
+		dalek.setSpeed(500);
+		dalek.hide();
+	for (int i = 0; i < 500; i++) {
 		
+		dalek.move(250);
+		dalek.turn(pie);
+		dalek.setRandomPenColor();
+	}
 	}
 }
